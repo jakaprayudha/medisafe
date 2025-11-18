0f1838502ad556efee5232be706202ce588c04f2
jakaprayudha <jakaprayudha3@gmail.com>
2025-11-18 14:14:14 +0700

    feat form persalinan

--- module/admin/pemeriksaan_inap.php
+++ module/admin/pemeriksaan_inap.php
@@ -170,6 +170,11 @@ $rme_type = $setting ? $setting['rme_type'] : 1; // default 1
                           <i class="bi bi-file-earmark-text me-2"></i>Foto Pasien
                         </a>
                       </li>
+                      <li>
+                        <a class="dropdown-item" href="module/admin/form_persalinan?no=${row.visit_ID}&rm=${row.nomor_rm}">
+                          <i class="bi bi-file-earmark-text me-2"></i>Rekapitulasi Pelayanan Persalinan
+                        </a>
+                      </li>
                         <li>
                         <a class="dropdown-item" href="module/admin/form_ranap?no=${row.visit_ID}&rm=${row.nomor_rm}">
                           <i class="bi bi-file-earmark-text me-2"></i>Keterangan Rawat Inap
