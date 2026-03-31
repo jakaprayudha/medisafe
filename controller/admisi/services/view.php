86662e9f71ec183e384ed8f2d7384d80571ad959
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-04-01 05:52:04 +0700

    service bpjs berdasarkan id customer

--- controller/admisi/services/view.php
+++ controller/admisi/services/view.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/../../../database/connect.php';
 session_start();
-$sql = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM `setting_pcare` WHERE id = 1"));
+$idcustomer = $_SESSION['id_customer'];
+$sql = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM `setting_pcare` WHERE id_customer = '$idcustomer'"));
 $base_url = $sql['base_url'];
 $service = $sql['service_name'];
 // date_default_timezone_set('Asia/Jakarta');
