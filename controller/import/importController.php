f4ea71b81d419b5d1bbdb45d9cab6da06d03f7be
Alfiandri Putra Perdana <alfiandri.putra@topremit.com>
2026-04-03 10:48:19 +0700

    chore: set response header json

--- controller/import/importController.php
+++ controller/import/importController.php
@@ -1,6 +1,6 @@
 <?php
 include '../../database/connect.php';
-
+header('Content-Type: application/json');
 $data = json_decode(file_get_contents("php://input"), true);
 
 $type = $data['type'] ?? null;
