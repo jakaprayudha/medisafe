0078951324b5387c6a63600075f8ba93665b7c58
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-04-07 12:45:16 +0700

    insert pasien jika data tidak ada di service pendaaftarn pasien

--- controller/admisi/pendaftaran.js
+++ controller/admisi/pendaftaran.js
@@ -30,13 +30,16 @@ $(function () {
                             APP.cetakhtml('#ppkumum', response.data.kdProviderPst['nmProvider']);
                             APP.cetakhtml('#noTelp', response.data.noHP);
                             APP.cetak('#noKartu', response.data.noKartu);
-                            if (response.data.noKTP == null){
-                                const nomor = $('#nomor').val();
-                                APP.cetakhtml('#noNIK', nomor);
+                            APP.cetak('#namapatient', response.data.nama);
+                            APP.cetak('#Kelamin', response.data.sex == "P" ? "Perempuan" : "Laki - Laki");
+                            APP.cetak('#tgllahir', response.data.tglLahir);
+                            if (response.data.noKTP == null || response.data.noKTP == ""){
+                                let nomor = $('#nomor').val();
                                 APP.cetak('#noNIK', nomor);
+                                APP.cetakhtml('#nonik', nomor);
                             }else{
                                 APP.cetak('#noNIK', response.data.noKTP);
-                                APP.cetakhtml('#noNIK', response.data.noKTP);
+                                APP.cetakhtml('#nonik', response.data.noKTP);
                             }
                             APP.cetak('#kdProviderPeserta', response.data.kdProviderPst['kdProvider']);
                         })
