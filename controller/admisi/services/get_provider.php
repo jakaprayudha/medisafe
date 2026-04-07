dd9bdceaca3909e06a0359e35251efd1bf6627bc
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-04-07 10:59:58 +0700

    tambah service insert pasien visit di service insert pendaftaran pcare

--- controller/admisi/services/get_provider.php
+++ controller/admisi/services/get_provider.php
@@ -1,16 +1,20 @@
 <?php
 require_once __DIR__ . '/view.php';
 header('Content-Type: application/json');
+$search = $_GET['search'] ?? '';
 $stmt = $koneksi->prepare("
     SELECT provider_name AS id, provider_name AS text 
-    FROM ms_provider
-    ORDER BY provider_name ASC
+    FROM ms_provider 
+    WHERE provider_name LIKE CONCAT('%', ?, '%')
     LIMIT 100
 ");
+
+$stmt->bind_param("s", $search);
 $stmt->execute();
-$result = $stmt->get_result();
+$hasil = $stmt->get_result();
+
 $data = [];
-while ($row = $result->fetch_assoc()) {
+while ($row = $hasil->fetch_assoc()){
     $data[] = $row;
 }
 echo json_encode($data);
