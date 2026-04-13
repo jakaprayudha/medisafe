18a2a65ae286328656bb12a56846abed16e67291
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-04-13 15:27:49 +0700

    testing

--- controller/admisi/api/getpeserta.php
+++ controller/admisi/api/getpeserta.php
@@ -2,13 +2,10 @@
 header('Content-Type: application/json');
 
 $response = [
-    "response" => [
-        "field"=> "noKunjungan",
-        "message"=> "0114U1630316Y000001"
-    ],
+    "response" => "/Lc1GVbcwa+XZHTMBHrVdNTReZuru7wpvlU59u3+ClMQIyxJHS0rXDHTe2nPzfxucqqRPhn60buc6IoXzM8dEI/JW2QB6yZeuJb/Q9vvU+E+aQub713p45cBeV2gXLRD",
     "metaData" => [
-        "message" => "CREATED",
-        "code" => 200
+        "message" => "Created",
+        "code" => 201
     ]
 ];
 
