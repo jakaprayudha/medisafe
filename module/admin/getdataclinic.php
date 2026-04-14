f5fcbe7fb4d80aff9db3f20f17138377fbfdc25b
jakaprayudha <jakaprayudha3@gmail.com>
2026-04-15 00:03:27 +0700

    fix:print surat pernyataan

--- module/admin/getdataclinic.php
+++ module/admin/getdataclinic.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $id_customer = $_SESSION['id_customer'];
 $checkfaseks = $koneksi->query("SELECT * FROM setting_clinic LEFT JOIN ms_faskes ON ms_faskes.id_clinic = setting_clinic.id  WHERE id_customer='$id_customer'");
 $datafaskes = $checkfaseks->fetch_assoc();
