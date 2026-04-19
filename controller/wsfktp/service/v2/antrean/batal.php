6662acfd632de764f0a02ceecb590ce6790f378a
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-04-20 01:54:22 +0700

    fix batal antrian v2 antrol

--- controller/wsfktp/service/v2/antrean/batal.php
+++ controller/wsfktp/service/v2/antrean/batal.php
@@ -1,8 +1,8 @@
 <?php
 header("Content-Type: application/json");
 
-require_once __DIR__ . '/../../../database/connect.php';
-require_once __DIR__ . '/../validateToken.php';
+require_once __DIR__ . '/../../../../../database/connect.php';
+require_once __DIR__ . '/../../../validateToken.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'PUT') {
     echo json_encode([
@@ -25,22 +25,67 @@ $kodepoli   = $data['kodepoli'] ?? null;
 $tanggal    = $data['tanggalperiksa'] ?? null;
 $keterangan = $data['keterangan'] ?? null;
 
-$stmt = $koneksi->prepare("DELETE FROM pasien_visit WHERE noKartu = ? AND id_poli = ? AND visit_date = ? AND id_customer = ?");
-$stmt->bind_param("ssss", $noKartu, $kodepoli, $tanggal, $id_customer);
-$result = $stmt->execute();
+$status_visit = '99';
 
-if ($result){
+$stmt = $koneksi->prepare("
+  SELECT pp.nomor_visit 
+  FROM pcare_pendaftaran AS pp 
+  INNER JOIN pasien_visit AS pv 
+    ON pp.nomor_visit = pv.visit_ID 
+  WHERE pv.noKartu = ? 
+    AND pp.tanggal_daftar = ? 
+    AND pp.kdPoli = ? 
+    AND (pv.visit_status IS NULL OR pv.visit_status != ?)
+  LIMIT 1
+");
+$stmt->bind_param("ssss", $noKartu, $tanggal, $kodepoli, $status_visit);
+$stmt->execute();
+$res = $stmt->get_result();
+if ($res->num_rows > 0) {
+    $row = $res->fetch_assoc();
+    $nomor_visit = $row['nomor_visit'];
+    $stmt = $koneksi->prepare("
+        UPDATE pasien_visit 
+        SET visit_status = ? 
+        WHERE noKartu = ? 
+        AND visit_date = ? 
+        AND id_customer = ?
+        AND visit_ID = ?
+    ");
+    $stmt->bind_param("sssss", $status_visit, $noKartu, $tanggal, $id_customer, $nomor_visit);
+    $result = $stmt->execute();
+
+    $status = '9';
+    $stmt1 = $koneksi->prepare("
+        UPDATE antrian_poli 
+        SET status = ? 
+        WHERE id_customer = ? 
+        AND poli = ? 
+        AND tanggal = ? 
+        AND nomor_visit = ?
+    ");
+    $stmt1->bind_param("sssss", $status, $id_customer, $kodepoli, $tanggal, $nomor_visit);
+    $result1 = $stmt1->execute();
+    if ($result && $result1) {
+        echo json_encode([
+            "metadata" => [
+                "message" => "Ok",
+                "code" => 200
+            ]
+        ]);
+    } else {
+        echo json_encode([
+            "metadata" => [
+                "message" => "Gagal update",
+                "code" => 500
+            ]
+        ]);
+    }
+} else {
     echo json_encode([
-    "metadata" => [
-        "message" => "Ok",
-        "code" => 200
-    ]
-]);
-}else{
-    echo json_encode([
-    "metadata" => [
-        "message" => "Error",
-        "code" => 201
-    ]
-]);
+        "metadata" => [
+            "message" => "Antrian tidak ditemukan",
+            "code" => 201
+        ]
+    ]);
 }
