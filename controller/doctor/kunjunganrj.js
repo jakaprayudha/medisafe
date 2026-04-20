4a33873826f0a14bc70349e439a7a4ab23b0272c
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-04-20 22:30:07 +0700

    penambahan logo bpjs pada pemeriksaan dokter

--- controller/doctor/kunjunganrj.js
+++ controller/doctor/kunjunganrj.js
@@ -61,6 +61,7 @@ $(function () {
                             $('#kdStatusPulang').val(d.kdStatusPulang).trigger('change');
                         })
                     } else {
+                        $('#logobpjs').removeClass('d-none');
                         if (d.noKunjungan == null) {
                             APP.cetak('#kode_poli', d.kdPoli);
                             APP.cetak('#nama_poli', d.nmPoli);
