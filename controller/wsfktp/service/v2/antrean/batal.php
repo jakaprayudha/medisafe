1f2cc118cf9b02ba89ea8e926683ba395bb600fb
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-05-08 18:30:55 +0700

    perbaikan revisi antrol ambil antian ulang jika visit sudah di batalkan

--- controller/wsfktp/service/v2/antrean/batal.php
+++ controller/wsfktp/service/v2/antrean/batal.php
@@ -31,6 +31,7 @@ try {
         WHERE noKartu = ?
         AND visit_date = ?
         AND id_customer = ?
+        AND visit_status != '99'
         LIMIT 1
     ");
     $stmt->bind_param("sss", $noKartu, $tanggal, $id_customer);
