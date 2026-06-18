5b477380d2cadbcc963949db191f5a28154c7b3f
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-06-18 18:53:45 +0700

    perbaikan pada validate nik

--- controller/admisi/pendaftaran.js
+++ controller/admisi/pendaftaran.js
@@ -77,7 +77,7 @@ $(function () {
     $('#pasienSelect').on('select2:select', function (e) {
         resetSemuaForm();
         let data = e.params.data;
-        const nomor = !data.nik ? data.no_bpjs : data.nik;
+        const nomor = data.nik == "" || data.nik == null || data.nik == '-' ? data.bpjs : data.nik;
         idPasien = data.id;
         nomorPasienAktif = nomor;
         loadPasien(nomor);
