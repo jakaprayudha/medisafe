4e8ca3e42f93dbd413aac24c1fcba04842e55d48
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-07-21 06:46:53 +0700

    penambaan validate disbles unutk jadwal dokter yang sudah exp

--- controller/admisi/services/getDokterlocal.php
+++ controller/admisi/services/getDokterlocal.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/view.php';
 header('Content-Type: application/json');
+date_default_timezone_set('Asia/Jakarta');
 $kdpoli = $_GET['kdpoli'];
 $tanggal = $_GET['tanggal'];
 $namaHari = [
@@ -16,13 +17,19 @@ $namaHari = [
 $hari = $namaHari[(new DateTime($tanggal))->format('l')];
 $sql = mysqli_query($koneksi, "SELECT d.doctor_name, d.doctor_code, j.day_of_week, j.start_time, j.end_time, db.nmDokter FROM ms_doctor AS d INNER JOIN ms_doctor_schedule AS j ON j.id_doctor = d.doctor_code INNER JOIN master_doctor_bpjs AS db ON db.kdDokter = d.doctor_code WHERE d.id_customer = '$idcustomer' AND j.id_customer = '$idcustomer' AND db.id_customer = '$idcustomer' AND j.day_of_week = '$hari' AND j.id_poli = '$kdpoli'");
 $data = [];
+$now = new DateTime();
 
 while ($row = mysqli_fetch_assoc($sql)){
+
+    $endPraktek = new DateTime($tanggal . ' ' . $row['end_time']);
+    $expired = $now > $endPraktek;
+
     $data[] = [
         "namadokter" => $row['doctor_name'],
         "kodedokter" => (int)$row['doctor_code'],
         "jampraktek" => $row['start_time'] . "-" . $row['end_time'],
-        "nmDokterBpjs" => $row['nmDokter']
+        "nmDokterBpjs" => $row['nmDokter'],
+        "exp" => $expired
     ];
 }
 
