f3d6e7ce0acc529b11ec7f5e9bde0b60e9061ac4
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-07-21 05:10:45 +0700

    perbaikan pada response error addAntrian

--- controller/wsbpjs/addAntrian.php
+++ controller/wsbpjs/addAntrian.php
@@ -88,8 +88,7 @@ try {
     exit;
 }
 
-function createAntrian($koneksi, $kdPoli, $idcustomer, $visit_ID, $kdDokter, $tglDaftarDB, $jampraktek)
-{
+function createAntrian($koneksi, $kdPoli, $idcustomer, $visit_ID, $kdDokter, $tglDaftarDB, $jampraktek){
     $cekantrian = $koneksi->prepare("SELECT 
         COALESCE(MAX(a.nomor), 0) AS last,
         (
@@ -125,8 +124,7 @@ function createAntrian($koneksi, $kdPoli, $idcustomer, $visit_ID, $kdDokter, $tg
         'display' => $kode_antrian . $next
     ];
 }
-function generateVisitID($koneksi, $idcustomer)
-{
+function generateVisitID($koneksi, $idcustomer){
     do {
         $date = date('ymd');
         $random = strtoupper(bin2hex(random_bytes(3)));
