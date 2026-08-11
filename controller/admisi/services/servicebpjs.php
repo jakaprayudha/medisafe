3bdaad14149689327541433986b031a77337179f
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-08-11 09:16:35 +0700

    perbaikan url icare dan perbaikan keluhan pada jkn

--- controller/admisi/services/servicebpjs.php
+++ controller/admisi/services/servicebpjs.php
@@ -84,8 +84,8 @@ function bpjsPostIcare(array $payload){
         "user_key: " . $userkey,
         "Content-Type: application/json",
     );
-    // $url = 'https://apijkn.bpjs-kesehatan.go.id/ihs/api/pcare/validate';
-    $url = 'https://apijkn-dev.bpjs-kesehatan.go.id/ihs_dev/api/pcare/validate';
+    $url = 'https://apijkn.bpjs-kesehatan.go.id/ihs/api/pcare/validate';
+    // $url = 'https://apijkn-dev.bpjs-kesehatan.go.id/ihs_dev/api/pcare/validate';
     $ch = curl_init();
     curl_setopt_array($ch, [
         CURLOPT_URL => $url,
@@ -100,7 +100,7 @@ function bpjsPostIcare(array $payload){
     $response = curl_exec($ch);
     $err = curl_error($ch);
     curl_close($ch);
-    // echo $response;die();
+    echo $response;die();
     // echo $err;die();
     if (!$response) {
         return bpjsError("Tidak ada response dari server BPJS");
