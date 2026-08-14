ae6051a2d0d731b23a903aceb1b1a1a91a5e95dd
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2026-08-14 16:19:40 +0700

    hidden form jadwal pada form kunjungan

--- module/admin/kunjungan.php
+++ module/admin/kunjungan.php
@@ -448,7 +448,7 @@ $rm = $_GET['rm'];
                           <div class="col-10">
                             <input type="text" id="nmfaskes" name="nmfaskes" class="form-control" readonly>
                             <input type="hidden" name="kdppk" id="kdfaskes">
-                            <input type="text" name="jadwal" id="jadwal">
+                            <input type="hidden" name="jadwal" id="jadwal">
                           </div>
                         </div>
                       </div>
