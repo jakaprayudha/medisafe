0226c5efad5fb93fb5a747335f9bf80722f4262e
jakaprayudha <jakaprayudha3@gmail.com>
2026-08-26 16:31:09 +0700

    feat:diagnosa icd or kdiag pcare surat keterangan sehat

--- module/letter/print/surat-keterangan-sehat.php
+++ module/letter/print/surat-keterangan-sehat.php
@@ -104,7 +104,11 @@
         mp.patient_address,
         pv.id_doctor,
         dc.sip_number,
-        pv.suhu
+        pv.suhu,
+        pv.nmDiag1,
+        pv.kdDiag1,
+        pv.diagnosa,
+        cd.icd10
      FROM surat_sehat ss
      INNER JOIN pasien_visit pv 
         ON pv.id_visit = ss.id_visit
@@ -112,6 +116,7 @@
         ON mp.id_patient = pv.id_patient
     LEFT JOIN ms_doctor dc 
         ON dc.doctor_name = pv.id_doctor
+    LEFT JOIN icd_10 cd ON cd.code = pv.diagnosa
      WHERE ss.id = '$id'
      LIMIT 1"
     );
@@ -287,6 +292,22 @@
           <td class="label">Alamat</td>
           <td class="separator">:</td>
           <td><?= $dataSurat['patient_address'] ?></td>
+        </tr>
+
+        <tr>
+          <td class="label">Diagnosa</td>
+          <td class="separator">:</td>
+          <td>
+            <?php
+            if (!empty($dataSurat['kdDiag1'])) {
+              echo $dataSurat['kdDiag1'] . ' - ' . ($dataSurat['nmDiag1'] ?? '-');
+            } elseif (!empty($dataSurat['diagnosa'])) {
+              echo $dataSurat['diagnosa'] . ' - ' . ($dataSurat['icd10'] ?? '-');
+            } else {
+              echo '-';
+            }
+            ?>
+          </td>
         </tr>
       </table>
 
