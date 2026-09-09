1dd185def1db4340a6b1a27c3991d7661be2cba0
jakaprayudha <jakaprayudha3@gmail.com>
2026-09-09 17:48:34 +0700

    feat:menu laporan farmasi roles farmasi module

--- module/admin/menu/menu-farmasi.php
+++ module/admin/menu/menu-farmasi.php
@@ -28,6 +28,23 @@
            </a>
         </li>
         <li class="sidebar-item">
+           <a class="sidebar-link <?php
+                                    if ($title == 'Laporan Farmasi') {
+                                       echo 'active';
+                                    }
+                                    ?>"
+              href="module/admin/rpt_farmasi"
+              aria-expanded="false">
+
+              <iconify-icon icon="mdi:clipboard-text-outline"></iconify-icon>
+
+              <span class="hide-menu">
+                 Laporan Stock Farmasi
+              </span>
+
+           </a>
+        </li>
+        <li class="sidebar-item">
            <a class="sidebar-link <?php if ($title == 'Farmasi Order') {
                                        echo 'active';
                                     } ?>"
